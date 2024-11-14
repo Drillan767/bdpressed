ec6c858a76b68be350e06f7284d39210ab9a1ef2
Drillan767 <jd.levarato@gmail.com>
2024-11-15 00:42:28 +0100

    Removed old stuff and added pinia

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,14 @@
 import { createApp } from 'vue'
-import './style.css'
 import App from './App.vue'
 import router from './plugins/router'
 import { Amplify } from 'aws-amplify'
+import { createPinia } from 'pinia'
 import outputs from '../amplify_outputs.json'
 
 Amplify.configure(outputs)
+const pinia = createPinia()
 
 createApp(App)
+    .use(pinia)
     .use(router)
     .mount('#app')
