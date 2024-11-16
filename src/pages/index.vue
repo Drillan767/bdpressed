74aff9e08ab417aa8a8dee6d12620861142f288e
Drillan767 <jd.levarato@gmail.com>
2024-11-16 05:18:57 +0100

    Clean up

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -1,18 +1,34 @@
 <script setup lang="ts">
+import useAuthStore from '@/stores/authStore'
+import { storeToRefs } from 'pinia'
 
+const { isAuthenticated, currentRole } = storeToRefs(useAuthStore())
 </script>
 
 <template>
     <div>
-        <h1>Hello World</h1>
-        <RouterLink :to="{ name: 'auth.login' }">
-            Login
-        </RouterLink><br>
-        <RouterLink :to="{ name: 'auth.register' }">
-            Register
-        </RouterLink><br>
-        <RouterLink :to="{ name: 'admin' }">
-            Admin / User
-        </RouterLink>
+        <h1>Bédé Primée 🗣️🗣️🔊🔊</h1>
+        <template v-if="isAuthenticated">
+            <RouterLink
+                v-if="currentRole === 'admin'"
+                :to="{ name: 'admin.dashboard' }"
+            >
+                Admin Dashboard
+            </RouterLink>
+            <RouterLink
+                v-if="currentRole === 'user'"
+                :to="{ name: 'user.dashboard' }"
+            >
+                User Dashboard
+            </RouterLink>
+        </template>
+        <template v-else>
+            <RouterLink :to="{ name: 'auth.login' }">
+                Connexion
+            </RouterLink><br>
+            <RouterLink :to="{ name: 'auth.register' }">
+                Inscription
+            </RouterLink><br>
+        </template>
     </div>
 </template>
--- src/pages/user/Dashboard.vue
+++ src/pages/user/Dashboard.vue
@@ -1,5 +1,21 @@
+<script setup lang="ts">
+import UserLayout from '@/layouts/UserLayout.vue'
+import { signOut } from 'aws-amplify/auth'
+import { useRouter } from 'vue-router'
+
+const router = useRouter()
+
+async function logout() {
+    await signOut()
+    router.push('/')
+}
+</script>
+
 <template>
-    <div>
+    <UserLayout>
         <h1>User Dashboard</h1>
-    </div>
+        <VBtn @click="logout">
+            Logout
+        </VBtn>
+    </UserLayout>
 </template>
