61eb113c43492b6948e3717fdc739576aa31dab3
Drillan767 <jd.levarato@gmail.com>
2024-11-16 05:03:49 +0100

    Auth related forms and pages. User can register and validate their mail

---
+++ src/pages/admin/Dashboard.vue
@@ -0,0 +1,21 @@
+<script setup lang="ts">
+import AdminLayout from '@/layouts/AdminLayout.vue'
+import { signOut } from 'aws-amplify/auth'
+import { useRouter } from 'vue-router'
+
+const router = useRouter()
+
+async function logout() {
+    await signOut()
+    router.push('/')
+}
+</script>
+
+<template>
+    <AdminLayout>
+        <h1>Admin Dashboard</h1>
+        <VBtn @click="logout">
+            Logout
+        </VBtn>
+    </AdminLayout>
+</template>
---
+++ src/pages/auth/Register.vue
@@ -0,0 +1,35 @@
+<script setup lang="ts">
+import RegisterForm from '@/components/auth/RegisterForm.vue'
+import RegisterOTP from '@/components/auth/RegisterOTP.vue'
+import AuthLayout from '@/layouts/AuthLayout.vue'
+import { ref } from 'vue'
+import { useRouter } from 'vue-router'
+
+const router = useRouter()
+
+const registerStatus = ref<'form' | 'otp'>('form')
+const registerEmail = ref('')
+
+function handleSuccess() {
+    router.push({ name: 'auth.login' })
+}
+</script>
+
+<template>
+    <AuthLayout>
+        <VTabsWindow v-model="registerStatus">
+            <VTabsWindowItem value="form">
+                <RegisterForm
+                    v-model="registerEmail"
+                    @success="registerStatus = 'otp'"
+                />
+            </VTabsWindowItem>
+            <VTabsWindowItem value="otp">
+                <RegisterOTP
+                    :email="registerEmail"
+                    @success="handleSuccess"
+                />
+            </VTabsWindowItem>
+        </VTabsWindow>
+    </AuthLayout>
+</template>
