54b5204425e283134a88f71fa61a2ecbbcbf8557
Drillan767 <jd.levarato@gmail.com>
2024-11-23 04:35:16 +0100

    ... Dynamic routes?

--- vite.config.ts
+++ vite.config.ts
@@ -1,4 +1,5 @@
 import vue from '@vitejs/plugin-vue'
+import VueRouter from 'unplugin-vue-router/vite'
 import { defineConfig } from 'vite'
 import vueDevTools from 'vite-plugin-vue-devtools'
 import vuetify from 'vite-plugin-vuetify'
@@ -6,6 +7,7 @@ import vuetify from 'vite-plugin-vuetify'
 // https://vite.dev/config/
 export default defineConfig({
     plugins: [
+        VueRouter({}),
         vue(),
         vuetify(),
         vueDevTools(),
