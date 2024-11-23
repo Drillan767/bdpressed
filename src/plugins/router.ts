91bc2f3b6f355dae1374f7436866dcf77b011b8e
Drillan767 <jd.levarato@gmail.com>
2024-11-23 04:35:16 +0100

    ... Dynamic routes?

--- src/plugins/router.ts
+++ src/plugins/router.ts
@@ -1,7 +1,7 @@
-import type { RouteRecordRaw } from 'vue-router'
 import { createRouter, createWebHistory } from 'vue-router'
+import { handleHotUpdate, routes } from 'vue-router/auto-routes'
 
-const routes: RouteRecordRaw[] = [
+/* const routes: RouteRecordRaw[] = [
     {
         path: '/',
         name: 'home',
@@ -17,11 +17,15 @@ const routes: RouteRecordRaw[] = [
         name: 'administration',
         component: () => import('@/pages/administration.vue'),
     },
-]
+] */
 
 const router = createRouter({
     history: createWebHistory(),
     routes,
 })
+
+if (import.meta.hot) {
+    handleHotUpdate(router)
+}
 
 export default router
