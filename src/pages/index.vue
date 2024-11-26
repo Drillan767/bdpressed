308405efe47086c23f90b2fdb8a0db6f40dc4bb1
Drillan767 <jd.levarato@gmail.com>
2024-11-27 00:58:03 +0100

    Upgraded vue-tsc by accident, rollback

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -1,7 +1,5 @@
 <script setup lang="ts">
 import VisitorLayout from '@/layouts/VisitorLayout.vue'
-import useAuthStore from '@/stores/authStore'
-import { storeToRefs } from 'pinia'
 
 definePage({
     meta: {
