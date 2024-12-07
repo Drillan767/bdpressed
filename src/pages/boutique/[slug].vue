951667e2bb894fdb7ed9d242bc810b5eb61ff402
Drillan767 <jd.levarato@gmail.com>
2024-12-07 21:33:52 +0100

    fetching by slug works, need to integrate a detail page

--- src/pages/boutique/[slug].vue
+++ src/pages/boutique/[slug].vue
@@ -1,9 +1,44 @@
 <script setup lang="ts">
+import useProductsStore from '@/stores/productsStore'
+import { useHead } from '@vueuse/head'
+import { storeToRefs } from 'pinia'
+import { onMounted, ref } from 'vue'
+import { useRoute, useRouter } from 'vue-router'
 
+const route = useRoute()
+const router = useRouter()
+const { params: { slug } } = route
+const { productBySlug } = useProductsStore()
+const { productsLoading } = storeToRefs(useProductsStore())
+const product = ref<any>()
+
+onMounted(async () => {
+    const result = await productBySlug(slug.toString())
+
+    if (result) {
+        product.value = result
+    }
+    else {
+        router.push({
+            name: 'NotFound',
+            params: { pathMatch: route.path.substring(1).split('/') },
+            query: route.query,
+            hash: route.hash,
+        })
+    }
+})
+
+useHead({
+    title: () => `Boutique | ${product.value?.name}`,
+})
 </script>
 
 <template>
     <div>
         <h1>Boutique</h1>
+    </div>
+    <div>
+        <p>{{ productsLoading ? 'loading...' : product?.name }}</p>
+        <p>{{ product }}</p>
     </div>
 </template>
