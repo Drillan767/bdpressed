256891b153684efaae2da6b2f22cc5f9de9c4bff
Drillan767 <jd.levarato@gmail.com>
2024-12-13 13:49:21 +0100

    Fixed responsive, removed unecessary component, applied consequences

--- src/pages/boutique/index.vue
+++ src/pages/boutique/index.vue
@@ -1,6 +1,5 @@
 <script setup lang="ts">
 import type { Catalog } from '@/types'
-import BedeBlock from '@/components/visitors/BedeBlock.vue'
 import useNumbers from '@/composables/numbers'
 import useProductsStore from '@/stores/productsStore'
 import { useHead } from '@vueuse/head'
@@ -28,107 +27,109 @@ onMounted(getProducts)
     <VContainer>
         <VRow>
             <VCol>
-                <BedeBlock>
-                    <h1 class="text-center text-h4 mb-4 bd">
-                        Mes petites créations à vendre :
-                    </h1>
-                    <h2 text-h5 class="text-center my-12 bd">
-                        ¥ Tu reçois des payettes, je reçois de quoi payer ma psy ¥
-                    </h2>
+                <VCard class="bede-block">
+                    <VCardText class="bede-text">
+                        <h1 class="text-center text-h4 mb-4 bd">
+                            Mes petites créations à vendre :
+                        </h1>
+                        <h2 text-h5 class="text-center my-12 bd">
+                            ¥ Tu reçois des payettes, je reçois de quoi payer ma psy ¥
+                        </h2>
 
-                    <VContainer>
-                        <VRow>
-                            <VCol
-                                cols="12"
-                                md="4"
-                            >
-                                <VCard
-                                    variant="flat"
-                                    to="/boutique/illustration"
+                        <VContainer>
+                            <VRow>
+                                <VCol
+                                    cols="12"
+                                    md="4"
                                 >
-                                    <VContainer class="pa-0 text-secondary">
-                                        <VRow no-gutters>
-                                            <VCol>
-                                                <VImg
-                                                    src="https://cdn.vuetifyjs.com/images/cards/sunshine.jpg"
-                                                    alt="Illustration"
-                                                    class="rounded-lg"
-                                                    aspect-ratio="9/16"
-                                                    width="100%"
-                                                />
-                                            </VCol>
-                                        </VRow>
-                                        <VRow>
-                                            <VCol cols="10">
-                                                <h3>Illustration</h3>
-                                                <p>À partir de 25€</p>
-                                            </VCol>
-                                            <VCol cols="2">
-                                                <VBtn
-                                                    variant="text"
-                                                    color="secondary"
-                                                    icon="mdi-cart"
-                                                />
-                                            </VCol>
-                                        </VRow>
-                                        <VRow no-gutters>
-                                            <VCol>
-                                                <p>
-                                                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Quidem, doloremque.
-                                                </p>
-                                            </VCol>
-                                        </VRow>
-                                    </VContainer>
-                                </VCard>
-                            </VCol>
-                            <VCol
-                                v-for="(product, i) in products"
-                                :key="i"
-                                cols="12"
-                                md="4"
-                            >
-                                <VCard
-                                    :to="`/boutique/${product.slug}`"
-                                    variant="flat"
+                                    <VCard
+                                        variant="flat"
+                                        to="/boutique/illustration"
+                                    >
+                                        <VContainer class="pa-0 text-secondary">
+                                            <VRow no-gutters>
+                                                <VCol>
+                                                    <VImg
+                                                        src="https://cdn.vuetifyjs.com/images/cards/sunshine.jpg"
+                                                        alt="Illustration"
+                                                        class="rounded-lg"
+                                                        aspect-ratio="9/16"
+                                                        width="100%"
+                                                    />
+                                                </VCol>
+                                            </VRow>
+                                            <VRow>
+                                                <VCol cols="10">
+                                                    <h3>Illustration</h3>
+                                                    <p>À partir de 25€</p>
+                                                </VCol>
+                                                <VCol cols="2">
+                                                    <VBtn
+                                                        variant="text"
+                                                        color="secondary"
+                                                        icon="mdi-cart"
+                                                    />
+                                                </VCol>
+                                            </VRow>
+                                            <VRow no-gutters>
+                                                <VCol>
+                                                    <p>
+                                                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Quidem, doloremque.
+                                                    </p>
+                                                </VCol>
+                                            </VRow>
+                                        </VContainer>
+                                    </VCard>
+                                </VCol>
+                                <VCol
+                                    v-for="(product, i) in products"
+                                    :key="i"
+                                    cols="12"
+                                    md="4"
                                 >
-                                    <VContainer class="pa-0 text-secondary">
-                                        <VRow no-gutters>
-                                            <VCol
-                                                :style="`height: ${smAndDown ? '200px' : '500px'}`"
-                                                class="image-container"
-                                            >
-                                                <img
-                                                    :src="product.promotedImage"
-                                                    :alt="product.name"
+                                    <VCard
+                                        :to="`/boutique/${product.slug}`"
+                                        variant="flat"
+                                    >
+                                        <VContainer class="pa-0 text-secondary">
+                                            <VRow no-gutters>
+                                                <VCol
+                                                    :style="`height: ${smAndDown ? '200px' : '500px'}`"
+                                                    class="image-container"
                                                 >
-                                            </VCol>
-                                        </VRow>
-                                        <VRow>
-                                            <VCol cols="10">
-                                                <h3>{{ product.name }}</h3>
-                                                <p>{{ formatPrice(product.price) }}</p>
-                                            </VCol>
-                                            <VCol cols="2">
-                                                <VBtn
-                                                    variant="text"
-                                                    color="secondary"
-                                                    icon="mdi-cart"
-                                                />
-                                            </VCol>
-                                        </VRow>
-                                        <VRow no-gutters>
-                                            <VCol>
-                                                <p>
-                                                    {{ product.quickDescription }}
-                                                </p>
-                                            </VCol>
-                                        </VRow>
-                                    </VContainer>
-                                </VCard>
-                            </VCol>
-                        </VRow>
-                    </VContainer>
-                </BedeBlock>
+                                                    <img
+                                                        :src="product.promotedImage"
+                                                        :alt="product.name"
+                                                    >
+                                                </VCol>
+                                            </VRow>
+                                            <VRow>
+                                                <VCol cols="10">
+                                                    <h3>{{ product.name }}</h3>
+                                                    <p>{{ formatPrice(product.price) }}</p>
+                                                </VCol>
+                                                <VCol cols="2">
+                                                    <VBtn
+                                                        variant="text"
+                                                        color="secondary"
+                                                        icon="mdi-cart"
+                                                    />
+                                                </VCol>
+                                            </VRow>
+                                            <VRow no-gutters>
+                                                <VCol>
+                                                    <p>
+                                                        {{ product.quickDescription }}
+                                                    </p>
+                                                </VCol>
+                                            </VRow>
+                                        </VContainer>
+                                    </VCard>
+                                </VCol>
+                            </VRow>
+                        </VContainer>
+                    </VCardText>
+                </VCard>
             </VCol>
         </VRow>
     </VContainer>
