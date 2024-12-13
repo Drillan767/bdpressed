36f2e3a3fadf4d02bc7fb71278a4f40d4fbeb3e3
Joseph Levarato <jd.levarato@gmail.com>
2024-12-13 13:50:43 +0100

    Revert "Fixed responsive, removed unecessary component, applied consequences"
    This reverts commit 263d958f8732286a7a3e8d8d85d708ffa0160964.

---
+++ src/components/visitors/BedeBlock.vue
@@ -0,0 +1,21 @@
+<template>
+    <VCard
+        class="bede-block"
+    >
+        <VCardText class="bede-text">
+            <slot />
+        </VCardText>
+    </VCard>
+</template>
+
+<style lang="scss" scoped>
+.bede-block {
+    border-radius: 48px !important;
+    box-shadow: 0 10px 10px 0 #891e00;
+
+    .bede-text {
+        padding: 3rem;
+        color: rgb(var(--v-theme-secondary));
+    }
+}
+</style>
