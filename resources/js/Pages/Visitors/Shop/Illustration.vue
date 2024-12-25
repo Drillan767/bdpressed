3261e3766c29b4dfbefc66b914ae7ef6188b289e
Drillan767 <jd.levarato@gmail.com>
2024-12-25 22:55:46 +0100

    Fixed future illustration page

---
+++ resources/js/Pages/Visitors/Shop/Illustration.vue
@@ -0,0 +1,14 @@
+<script setup lang="ts">
+import VisitorsLayout from '@/Layouts/VisitorsLayout.vue'
+import { useHead } from '@vueuse/head'
+
+defineOptions({ layout: VisitorsLayout })
+
+useHead({
+    title: 'Commander une illustration',
+})
+</script>
+
+<template>
+    <h1>Illustration</h1>
+</template>
