ec0e129e13be846b7873eda2eff34dc9d5062dee
Drillan767 <jd.levarato@gmail.com>
2024-12-30 02:40:45 +0100

    Added admin templates for an order's detail

---
+++ resources/js/Pages/Admin/Orders/Index.vue
@@ -0,0 +1,42 @@
+<script setup lang="ts">
+import AdminLayout from '@/Layouts/AdminLayout.vue'
+import { useHead } from '@vueuse/head'
+import { ref } from 'vue'
+import { useDisplay } from 'vuetify'
+
+defineOptions({ layout: AdminLayout })
+
+useHead({
+    title: 'Commandes',
+})
+</script>
+
+<template>
+    <h1>Commandes</h1>
+    <VDataTable
+        :headers="[
+            {
+                title: 'Commande',
+                key: 'id',
+                sortable: true,
+            },
+            {
+                title: 'Statut',
+                key: 'status',
+                sortable: true,
+            },
+            {
+                title: 'Date',
+                key: 'created_at',
+                sortable: true,
+            },
+            {
+                title: 'Actions',
+                key: 'actions',
+                sortable: false,
+                align: 'end',
+            },
+        ]"
+        :items="[]"
+    />
+</template>
---
+++ resources/js/Pages/Admin/Orders/Show.vue
@@ -0,0 +1,16 @@
+<script setup lang="ts">
+import AdminLayout from '@/Layouts/AdminLayout.vue'
+import { useHead } from '@vueuse/head'
+import { ref } from 'vue'
+import { useDisplay } from 'vuetify'
+
+defineOptions({ layout: AdminLayout })
+
+useHead({
+    title: 'Commande',
+})
+</script>
+
+<template>
+    <h1>Commande</h1>
+</template>
