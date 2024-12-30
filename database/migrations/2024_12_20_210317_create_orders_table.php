8f1d3dcd61e92453aa8580a8bca24c3d7efe58fb
Drillan767 <jd.levarato@gmail.com>
2024-12-30 02:37:59 +0100

    Cleaned up migrations and added more stuff to seeders

--- database/migrations/2024_12_20_210317_create_orders_table.php
+++ database/migrations/2024_12_20_210317_create_orders_table.php
@@ -14,6 +14,7 @@ return new class extends Migration
         Schema::create('orders', function (Blueprint $table) {
             $table->id();
             $table->float('total', 2);
+            $table->string('reference');
             $table->text('additionalInfos')->nullable();
             $table->foreignId('user_id')->constrained('users');
             $table->boolean('useSameAddress')->default(true);
