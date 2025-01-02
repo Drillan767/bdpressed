731d33815c19007cc5c3a9abc2930789934b6c84
Drillan767 <jd.levarato@gmail.com>
2025-01-02 16:26:48 +0100

    Added enums and improved models

--- app/Models/OrderDetail.php
+++ app/Models/OrderDetail.php
@@ -5,10 +5,27 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
+/**
+ * @property int $order_id
+ * @property int $quantity
+ * @property float price
+ * @property string $created_at
+ * @property string $updated_at
+ */
 class OrderDetail extends Model
 {
+    protected $casts = [
+        'created_at' => 'datetime:d/m/Y H:i',
+        'updated_at' => 'datetime:d/m/Y H:i',
+    ];
+
     public function product(): belongsTo
     {
         return $this->belongsTo(Product::class);
+    }
+
+    public function order(): belongsTo
+    {
+        return $this->belongsTo(Order::class);
     }
 }
