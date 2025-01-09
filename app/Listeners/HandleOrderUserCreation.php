101e9f1b259059d6ec5abbda01e18d686aa97a29
Drillan767 <jd.levarato@gmail.com>
2025-01-09 10:24:19 +0100

    Created event and listeners for orders

---
+++ app/Listeners/HandleOrderUserCreation.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Listeners;
+
+use App\Events\OrderCreated;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Queue\InteractsWithQueue;
+use Illuminate\Auth\Events\Registered;
+
+class HandleOrderUserCreation implements ShouldQueue
+{
+    /**
+     * Create the event listener.
+     */
+    public function __construct()
+    {
+        //
+    }
+
+    /**
+     * Handle the event.
+     */
+    public function handle(OrderCreated $event): void
+    {
+        if ($event->accountCreated) {
+            event(new Registered($event->order->user));
+        }
+    }
+}
