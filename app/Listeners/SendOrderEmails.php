fb26f9e2e2bca84f069ce042b311a39686cc38b4
Drillan767 <jd.levarato@gmail.com>
2025-01-09 10:24:19 +0100

    Created event and listeners for orders

---
+++ app/Listeners/SendOrderEmails.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Listeners;
+
+use App\Events\OrderCreated;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use App\Notifications\NewOrderNotification;
+use App\Notifications\OrderConfirmationNotification;
+use Illuminate\Support\Facades\Notification;
+
+class SendOrderEmails implements ShouldQueue
+{
+    /**
+     * Create the event listener.
+     */
+    public function __construct()
+    {
+        //
+    }
+
+    /**
+     * Handle the event.
+     */
+    public function handle(OrderCreated $event): void
+    {
+        
+        if ($event->order->user) {
+            $event->order->user->notify(new NewOrderNotification($event->order));
+        }
+
+        if ($event->order->guest) {
+            Notification::route('mail', $event->order->guest->email)->notify(new OrderConfirmationNotification($event->order));
+        }
+
+        Notification::route('mail', config('mail.admin'))->notify(new NewOrderNotification($event->order));
+    }
+}
