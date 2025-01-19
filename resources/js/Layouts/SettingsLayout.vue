b72cdff542fe92a1f4a41f4b65ccead0a4b6c559
Drillan767 <jd.levarato@gmail.com>
2025-01-19 19:17:12 +0100

    Started working on form

---
+++ resources/js/Layouts/SettingsLayout.vue
@@ -0,0 +1,22 @@
+<template>
+    <VCard
+        prepend-icon="mdi-cog-outline"
+        title="Paramètres"
+    >
+        <template #text>
+            <VTabs>
+                <VTab
+                    prepend-icon="mdi-application-cog-outline"
+                    text="Site"
+                    value="site"
+                />
+                <VTab
+                    prepend-icon="mdi-palette-outline"
+                    text="Illustrations"
+                />
+            </VTabs>
+
+            <slot />
+        </template>
+    </VCard>
+</template>
