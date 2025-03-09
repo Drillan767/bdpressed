3bccb3d2008561d35c368918ac0bf598e453949f
Drillan767 <jd.levarato@gmail.com>
2025-03-09 17:10:03 +0100

    Added more status for illustrations

--- app/Enums/IllustrationStatus.php
+++ app/Enums/IllustrationStatus.php
@@ -5,6 +5,12 @@ namespace App\Enums;
 enum IllustrationStatus: string
 {
     case PENDING = 'PENDING';
+    case ACCEPTED = 'ACCEPTED';
+    case REJECTED = 'REJECTED';
+    case DEPOSIT_PENDING = 'DEPOSIT_PENDING';
+    case DEPOSIT_PAID = 'DEPOSIT_PAID';
+    case PAYMENT_PENDING = 'PAYMENT_PENDING';
+    case PAID = 'PAID';
     case IN_PROGRESS = 'IN_PROGRESS';
     case COMPLETED = 'COMPLETED';
 }
