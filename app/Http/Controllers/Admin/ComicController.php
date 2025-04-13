dbd063e447023955fa455f1b3abbdfba2347d6e9
Drillan767 <jd.levarato@gmail.com>
2025-04-13 16:34:55 +0200

    WIP

--- app/Http/Controllers/Admin/ComicController.php
+++ app/Http/Controllers/Admin/ComicController.php
@@ -7,12 +7,13 @@ use Inertia\Inertia;
 use Inertia\Response;
 use App\Http\Requests\ComicRequest;
 use App\Http\Controllers\Controller;
-
+use App\Models\Comic;
 class ComicController extends Controller
 {
     public function index(): Response
     {
-        return Inertia::render('Comics/Index');
+        $comics = Comic::all();
+        return Inertia::render('Admin/Comics/Index', compact('comics'));
     }
 
     public function store(ComicRequest $request)
---
+++ resources/js/Components/Admin/Comics/CreateComicDialog.vue
@@ -0,0 +1,37 @@
+<script setup lang="ts">
+import ComicForm from './ComicForm.vue'
+
+const displayDialog = defineModel<boolean>({ required: true })
+</script>
+
+<template>
+    <VDialog
+        v-model="displayDialog"
+        max-width="960"
+        persistent
+        scrollable
+    >
+        <VCard
+            title="Créer une BD"
+            prepend-icon="mdi-draw"
+        >
+            <template #text>
+                <ComicForm />
+            </template>
+            <template #actions>
+                <VBtn
+                    variant="text"
+                    @click="displayDialog = false"
+                >
+                    Annuler
+                </VBtn>
+                <VBtn
+                    variant="flat"
+                    color="primary"
+                >
+                    Créer
+                </VBtn>
+            </template>
+        </VCard>
+    </VDialog>
+</template>
