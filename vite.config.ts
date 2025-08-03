16938a25c47b7e8aa32210dbd47aa87bd68b4b98
Drillan767 <jd.levarato@gmail.com>
2025-08-03 18:30:28 +0200

    Fixed alias

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,12 @@
+import path from 'node:path'
+import { fileURLToPath } from 'node:url'
 import vue from '@vitejs/plugin-vue'
 import laravel from 'laravel-vite-plugin'
 import { defineConfig } from 'vite'
 import vuetify from 'vite-plugin-vuetify'
+
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = path.dirname(__filename)
 
 export default defineConfig({
     plugins: [
@@ -24,6 +29,11 @@ export default defineConfig({
             },
         }),
     ],
+    resolve: {
+        alias: {
+            '@': path.resolve(__dirname, './resources/js'),
+        },
+    },
     ssr: {
         noExternal: [
             '@inertiajs/server',
