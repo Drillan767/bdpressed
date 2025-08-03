c538856ece3193a96fea4c3827f46af9d7777b52
Drillan767 <jd.levarato@gmail.com>
2025-08-03 18:30:50 +0200

    Illustration details, WIP for now

---
+++ resources/js/Pages/Admin/Illustrations/Index.vue
@@ -0,0 +1,17 @@
+<script setup lang="ts">
+import type { IllustrationSpecs } from '@/types'
+
+defineProps<{
+    illustrations: IllustrationSpecs[]
+}>()
+</script>
+
+<template>
+    <VContainer>
+        <VRow>
+            <VCol>
+                Illustrations
+            </VCol>
+        </VRow>
+    </VContainer>
+</template>
