c26937d623e8799b6f9522f6ed8b0100947a5930
Drillan767 <jd.levarato@gmail.com>
2025-08-11 23:44:05 +0200

    Deploy command

---
+++ app/Console/Commands/DeployCommand.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Console\Commands;
+
+use Illuminate\Console\Command;
+
+class DeployCommand extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'bdp:deploy';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Runs post deploy commands';
+
+    /**
+     * Execute the console command.
+     */
+    public function handle(): void
+    {
+        // php artisan config:cache && php artisan route:cache && php artisan migrate --force --seed && php artisan storage:link
+        $this->info('Running post deploy commands');
+        $this->call('config:cache');
+        $this->call('route:cache');
+        $this->call('migrate', ['--force' => true, '--seed' => true]);
+        $this->call('storage:link');
+        $this->info('Done.');
+    }
+}
