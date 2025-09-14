80fd9df482d351fc7675fe0fc54aaba639b5f6d1
Drillan767 <jd.levarato@gmail.com>
2025-09-14 17:09:45 +0200

    Removed ApexCharts to go back to VPie, made it work

--- resources/js/Components/Admin/Statistics/Doughnut.vue
+++ resources/js/Components/Admin/Statistics/Doughnut.vue
@@ -1,49 +1,73 @@
 <script setup lang="ts">
+import type { ChartData } from '@/types/statistics'
 import { computed } from 'vue'
-import VueApexCharts from 'vue3-apexcharts'
+import { useDisplay } from 'vuetify'
 
-interface Props {
-    series: number[]
-    colors: string[]
-    labels: string[]
-    centerValue: number
-}
+defineProps<ChartData>()
 
-const props = defineProps<Props>()
+const tooltip = { subtitleFormat: '[value]%' }
 
-const chartOptions = computed(() => ({
-    chart: {
-        type: 'donut',
-        fontFamily: 'inherit',
-    },
-    dataLabels: {
-        enabled: true,
-    },
-    plotOptions: {
-        pie: {
-            donut: {
-                size: '50%',
-                labels: {
-                    show: true,
-                    total: {
-                        show: true,
-                        showAlways: true,
-                        label: 'Commandes',
-                        formatter: () => props.centerValue,
-                    },
-                },
-            },
-        },
-    },
-    colors: props.colors,
-    labels: props.labels,
-}))
+const { mobile } = useDisplay()
+
+const legend = computed<{ position: 'right' | 'bottom' }>(() => {
+    return {
+        position: mobile.value ? 'bottom' : 'right',
+    }
+})
 </script>
 
 <template>
-    <VueApexCharts
-        type="donut"
-        :options="chartOptions"
-        :series="series"
-    />
+    <VPie
+        :items
+        :legend
+        :tooltip
+        inner-cut="70"
+        item-key="id"
+        gap="2"
+        rounded="2"
+        reveal
+        animation
+        hide-slice
+    >
+        <template #center>
+            <div class="text-center">
+                <div class="text-h5">
+                    {{ centerText }}
+                </div>
+                <div class="opacity-70 mt-1 mb-n1">
+                    {{ centerLabel }}
+                </div>
+            </div>
+        </template>
+        <template #legend="{ items: legends, isActive, toggle }">
+            <VList
+                class="py-0 mb-n5 mb-md-0 bg-transparent"
+                density="compact"
+                width="300"
+            >
+                <VListItem
+                    v-for="item in legends"
+                    :key="item.key"
+                    :class="{ 'opacity-40': !isActive(item) }"
+                    :title="item.title"
+                    class="my-1"
+                    rounded="lg"
+                    link
+                    @click="toggle(item)"
+                >
+                    <template #prepend>
+                        <VAvatar
+                            :color="item.color"
+                            :size="16"
+                        />
+                    </template>
+                    <template #append>
+                        <div class="font-weight-bold">
+                            {{ item.value }}%
+                        </div>
+                    </template>
+                </VListItem>
+            </VList>
+        </template>
+    </VPie>
 </template>
