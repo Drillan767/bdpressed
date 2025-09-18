01e6eced62ddd100d96939a18f8a1e548c34f18d
Drillan767 <jd.levarato@gmail.com>
2025-09-18 02:08:48 +0200

    Fixed test

--- tests/Feature/Auth/RegistrationTest.php
+++ tests/Feature/Auth/RegistrationTest.php
@@ -22,5 +22,5 @@ test('new users can register', function () {
     ]);
 
     $this->assertAuthenticated();
-    $response->assertRedirect(route('user.dashboard', absolute: false));
+    $response->assertRedirect(route('verification.notice', absolute: false));
 });
